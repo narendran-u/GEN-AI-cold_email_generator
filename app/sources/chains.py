64138ecf6b02c5c68fbfc3425b482cb884c45cb5
narendran-u <narenshankar2004@gmail.com>
2024-11-06 21:43:12 +0530

    complete project

--- app/sources/chains.py
+++ app/sources/chains.py
@@ -39,14 +39,14 @@ class Chain:
             {job_description}
 
             ### INSTRUCTION:
-            You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
+            You are Mohan, a business development executive at abc.pvt.ltd abc.pvt.ltd is an AI & Software Consulting company dedicated to facilitating
             the seamless integration of business processes through automated tools. 
             Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
             process optimization, cost reduction, and heightened overall efficiency. 
-            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
+            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of abc.pvt.ltd 
             in fulfilling their needs.
             Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
-            Remember you are Mohan, BDE at AtliQ. 
+            Remember you are Mohan, BDE at abc.pvt.ltd 
             Do not provide a preamble.
             ### EMAIL (NO PREAMBLE):
 
